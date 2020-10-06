2fac14e994b360eb5f869fb7473b710c5b4f2e9d
Nicholas K <p21nkatsetos@fairfieldprep.org>
2020-10-06 09:08:43 -0400

    Submit cycle 4 day 2 labs

---
+++ lab_2-1.py
@@ -0,0 +1,5 @@
+# Author: NJK (AMDG) 10/6/20
+
+a = input("Input an integer. ")
+if int(a) % 2 == 0:
+    print("a is an even number.")
---
+++ lab_2-2.py
@@ -0,0 +1,7 @@
+# Author: NJK (AMDG) 10/6/20
+
+a = input("Input an integer. ")
+if int(a) % 2 == 0:
+    print(a + " is an even number.")
+else:
+    print(str(a) + " is an odd number.")
